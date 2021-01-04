545db45657b39c19c875a135c1d7d8f03bc8530d
Samuele Landi <samuele.landi@kryptotel.net>
2021-01-04 21:33:36 +0400

    current version

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -61,6 +61,8 @@ pub fn development_config() -> Result<ChainSpec, String> {
 				get_account_id_from_seed::<sr25519::Public>("Bob"),
 				get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
 				get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+				get_account_id_from_seed::<sr25519::Public>("Admin"),
+
 			],
 			true,
 		),
@@ -109,6 +111,8 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 				get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
 				get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
 				get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
+				get_account_id_from_seed::<sr25519::Public>("Admin"),
+
 			],
 			true,
 		),
